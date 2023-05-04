a19ffc8502853821b147c2658264101c5956bdab
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-05-04 21:38:28 +0330

    Add compose preview to sample app

--- sample/src/main/java/com/hamid97m/herodatepicker/sample/MainActivity.kt
+++ sample/src/main/java/com/hamid97m/herodatepicker/sample/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.hamid97m.herodatepicker.model.SelectedDate
 import com.hamid97m.herodatepicker.sample.theme.HeroDatePickerTheme
 import com.hamid97m.herodatepicker.view.HeroDatePicker
@@ -20,19 +23,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HeroDatePickerTheme {
-                var selectedDateState by remember { mutableStateOf(SelectedDate(0, 0, 0)) }
-                Column {
-                    HeroDatePicker(modifier = Modifier.fillMaxWidth()) { selectedDate ->
-                        /*
-                        selectedDate.year
-                        selectedDate.month
-                        selectedDate.day
-                        */
-                        selectedDateState = selectedDate
-                    }
-                    Text(text = "${selectedDateState.year}/${selectedDateState.month}/${selectedDateState.day}")
+                Surface {
+                    MainScreen()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    var selectedDateState by remember { mutableStateOf(SelectedDate(0, 0, 0)) }
+    Column {
+        HeroDatePicker(modifier = Modifier.fillMaxWidth()) { selectedDate ->
+            /*
+            selectedDate.year
+            selectedDate.month
+            selectedDate.day
+            */
+            selectedDateState = selectedDate
+        }
+        Text(text = "${selectedDateState.year}/${selectedDateState.month}/${selectedDateState.day}")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    HeroDatePickerTheme {
+        MainScreen()
     }
 }
