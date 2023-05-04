186bb74751d152b8f90a5190799fcd19eeb78879
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-05-04 21:52:00 +0330

    Improve sample app UI

--- sample/src/main/java/com/hamid97m/herodatepicker/sample/MainActivity.kt
+++ sample/src/main/java/com/hamid97m/herodatepicker/sample/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -12,8 +14,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hamid97m.herodatepicker.model.SelectedDate
 import com.hamid97m.herodatepicker.sample.theme.HeroDatePickerTheme
 import com.hamid97m.herodatepicker.view.HeroDatePicker
@@ -33,17 +37,24 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun MainScreen() {
-    var selectedDateState by remember { mutableStateOf(SelectedDate(0, 0, 0)) }
-    Column {
-        HeroDatePicker(modifier = Modifier.fillMaxWidth()) { selectedDate ->
-            /*
-            selectedDate.year
-            selectedDate.month
-            selectedDate.day
-            */
+    var selectedDateState by remember {
+        mutableStateOf(SelectedDate(0, 0, 0))
+    }
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        HeroDatePicker { selectedDate ->
             selectedDateState = selectedDate
         }
-        Text(text = "${selectedDateState.year}/${selectedDateState.month}/${selectedDateState.day}")
+
+        val (year, month, day) = selectedDateState
+        Text(
+            text = "$year/$month/$day",
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.h6
+        )
     }
 }
 
